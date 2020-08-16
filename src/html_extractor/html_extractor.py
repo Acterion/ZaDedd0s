aa663c78c8e2d4283e40ba3f5ee5733f4da0da5e
Acterion <nikich375@gmail.com>
2020-08-17 01:19:59 +0500

    Revert "Removed re"
    This reverts commit 7f6b873b2a805d568475ea40856882f749b10a61.

--- src/html_extractor/html_extractor.py
+++ src/html_extractor/html_extractor.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
 
+import re
 
 only_captcha_tags = SoupStrainer("captcha")
 only_a_tags = SoupStrainer("a")
@@ -34,4 +35,4 @@ class HtmlExtractor:
 
     def check_success(self, html):
         soup = BeautifulSoup(html, 'html.parser', parse_only=only_p_tags)
-        return False if "ошибка" in soup.p.text else True
+        return False if soup.find(text=re.compile('ошибка')) else True
