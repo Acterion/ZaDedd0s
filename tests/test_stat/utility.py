d738b8a42e7f0820c750e81a2601ad07f987aedb
Acterion <nikich375@gmail.com>
2020-08-24 02:35:53 +0500

    Stat tests revised v2

--- tests/test_stat/utility.py
+++ tests/test_stat/utility.py
@@ -1,7 +1,5 @@
 from src.stat.stat import StatisticsCollector
-from src.stat.data import StatData, Report
 import time
-from unittest import mock
 
 
 class MStatisticsCollector(StatisticsCollector):
@@ -13,11 +11,24 @@ class MStatisticsCollector(StatisticsCollector):
         return None
 
 
-prefilled_stat_data = StatData('test_stat_filled.txt')
+filled_dict = {
+    "avg_captcha_count": 1,
+    "avg_day_cost": 2,
+    "balance": 3,
+    "current_state": True,
+    "page_updates": 4,
+    "rejected": 5,
+    "spent": 6,
+    "total_captcha_count": 7,
+    "uptime": 1000,
+    "uptime_captcha_count": 8,
+    "uptime_end": 2000,
+    "uptime_spent": 9,
+    "uptime_start": 1000
+}
 
-prefilled_report = Report(7, 5, 6, 9.9944, 4, "Up", round(time.time()) - 999, 1, 2)
 
-filled = '''{
+filled_json = '''{
     "avg_captcha_count": 1,
     "avg_day_cost": 2,
     "balance": 3,
@@ -31,4 +42,20 @@ filled = '''{
     "uptime_end": 2000,
     "uptime_spent": 9,
     "uptime_start": 1000
+}'''
+
+empty_json = '''{
+    "avg_captcha_count": 0,
+    "avg_day_cost": 0,
+    "balance": 0,
+    "current_state": false,
+    "page_updates": 0,
+    "rejected": 0,
+    "spent": 0,
+    "total_captcha_count": 0,
+    "uptime": 0,
+    "uptime_captcha_count": 0,
+    "uptime_end": 0,
+    "uptime_spent": 0,
+    "uptime_start": 0
 }'''
