2863366db27669fd3eade808695a0c5c0a2d080a
Ali <deneb.25@ya.ru>
2020-08-25 01:21:00 +0500

    time check rounded in tests

--- tests/test_operator/test_operator_clocks.py
+++ tests/test_operator/test_operator_clocks.py
@@ -18,8 +18,8 @@ def test_clock_raises_exception_if_too_early():
 
 
 def test_shutdown_and_next_start_times():
-    now = datetime.now().replace(hour=10)
+    now = datetime.now().replace(hour=10, microsecond=0)
     clocks = OperatorClocks(now=now)
     shutdown, start = clocks.shutdown_and_next_start_times(now.time())
-    assert shutdown == datetime.now().replace(hour=20)
-    assert start == datetime.now().replace(day=datetime.now().day + 1, hour=8)
+    assert shutdown == datetime.now().replace(hour=20, microsecond=0)
+    assert start == datetime.now().replace(day=datetime.now().day + 1, hour=8, microsecond=0)
